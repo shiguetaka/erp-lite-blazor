615d5fe04c4d32bc6c6839387ceb4ce884b916a6
shiguetaka <kojijane@hotmail.com>
2026-06-21 14:05:59 -0400

    Add Entity Framework and Cliente database migration

---
+++ ERP.Domain/Entities/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace ERP.Domain.Entities;
+
+public class Cliente
+{
+    public Guid Id { get; set; }
+
+    public string Nome { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Telefone { get; set; } = string.Empty;
+
+    public DateTime DataCadastro { get; set; }
+        = DateTime.UtcNow;
+}
---
+++ ERP.Infrastructure/Data/ERPDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ERP.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP.Infrastructure.Data;
+
+public class ERPDbContext : DbContext
+{
+    public ERPDbContext(
+        DbContextOptions<ERPDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Cliente> Clientes { get; set; }
+}
